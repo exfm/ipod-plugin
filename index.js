7f2426495af9db9d6a6f0a186e73f8536b5d0c93
Dan Kantor <dan@ex.fm>
2012-11-16 12:58:24 -0500

    added getSongs method

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-(function(){
-    "use strict";
+/**
+ * This provides access to iPod Songs
+ * In Cordova.plist/Plugins, add this mapping (key:iPodPlugin, value:iPodPlugin)
+ */
 
-    // Attach methods you want to expose to root.
-    var root = (typeof module !== "undefined") ? module.exports : window;
-}());
+(function(){
+
+
+// constructor
+function iPodPlugin(){
+    console.log('iPodPlugin installed');
+}
+
+// get songs
+iPodPlugin.prototype.getSongs = function(callback){
+    cordovaRef.exec(callback, null, "iPodPlugin", "songs", []);
+};
+
+
+if(typeof module !== "undefined"){
+    module.exports = iPodPlugin;
+}
+else{
+    window.iPodPlugin = iPodPlugin;
+}
+
+}()); // end wrapper
