a6ecddb5f86ab7309835ddcb75b76958177f276d
Dan Kantor <dan@ex.fm>
2012-12-06 14:37:35 -0500

    updated for new functionality

--- index.js
+++ index.js
@@ -12,9 +12,27 @@ function IPodPlugin(){
     console.log('IPodPlugin installed');
 }
 
-// get songs
-IPodPlugin.prototype.getSongs = function(callback){
-    cordovaRef.exec(callback, null, "iPodPlugin", "songs", []);
+
+//It is required to call the update method at least once, on app start. 
+//It will return a JS object with an event name. There are two possible events: libraryupdating and librarychanged. 
+//The libraryupdating event means the iOS device is currently syncing with iTunes. 
+//This should never happen on app start. The librarychanged event means syncing is done. 
+//This event will have an additional field, count, with the number of songs in the library. 
+//This can be caused with the getShuffledSongs call so you don't run off the end of the library.
+
+IPodPlugin.prototype.update = function(callback) {
+    cordovaRef.exec(callback, null, 'iPodPlugin', 'update', []);
+};
+
+// get a list of shuffled songs
+IPodPlugin.prototype.getShuffledSongs = function(callback, start, offset) {
+    cordovaRef.exec(callback, null, 'iPodPlugin', 'songs', [start, offset]);
+};
+
+// The getArtwork method will return a complete base64 encoded image string
+// ready to insert into a src attribute of an img tag.
+IPodPlugin.prototype.getArtwork = function(callback, songId, width, height) {
+    cordovaRef.exec(callback, null, 'iPodPlugin', 'artwork', [songId, width, height]);
 };
 
 
